aa1d37483bd27078e01a431dad5d0d6d60f68730
kameshwarchurendra <kameshwarchurendra1995@gmail.com>
2024-03-16 22:39:02 +0530

    Update file

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Add Employee Form</title>
+  <title>Edit Employee Form</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -15,20 +15,35 @@
   <form action="{{ url('update') }}" method="post">
     @csrf
   <div class="form-group">
-      <label for="text">Name:</label>
+      <label for="text">Name<span style="color:red">*</span></label>
       <input type="text" class="form-control" id="name"  value="{{  $employee->name }}" placeholder="Enter Name" name="name">
+      @error('name')
+        <div style="color:red">
+            {{$message}}
+        </div>
+      @enderror
     </div>
     <div class="form-group">
-      <label for="email">Email:</label>
+      <label for="email">Email<span style="color:red">*</span></label>
       <input type="email" class="form-control" id="email"  value="{{  $employee->email }}" placeholder="Enter email" name="email">
+      @error('email')
+        <div style="color:red">
+            {{$message}}
+        </div>
+      @enderror
     </div>
     <div class="form-group">
-      <label for="Phone">Phone:</label>
+      <label for="Phone">Phone<span style="color:red">*</span></label>
       <input type="number" class="form-control" id="phone"  value="{{  $employee->phone }}" placeholder="Enter phone Number" name="phone">
+      @error('phone')
+        <div style="color:red">
+            {{$message}}
+        </div>
+      @enderror
     </div>
     <input type="hidden" class="form-control" id="id"  value="{{$employee->id}}"  name="id">
 
-    <button type="submit" class="btn btn-default">Update</button>
+    <button type="submit" class="btn btn-primary">Update</button>
   </form>
   @endif
 </div>
